7c7b0876b4d2027b6e6f469bb110d129cd9bbfda
na2hiro <a@81.la>
2013-11-08 05:23:57 +0900

    doMove, othelloひと通り

--- othello/othello.hs
+++ othello/othello.hs
@@ -1,15 +1,22 @@
 import Debug.Trace(trace)
 import Data.Array
+import Data.List(intercalate)
 
-data Color = Black | White deriving(Show, Eq)
+data Color = Black | White deriving(Eq)
+instance Show Color where
+    show Black = "o"
+    show White = "x"
 
 type Coord = (Int,Int) 
 
 type Board = Array Coord (Maybe Color)
 type Ply = Int
 type Counts = (Int,Int)
-data Othello = Othello Board Ply Counts deriving(Show)
-data Move = Move Coord [Coord] deriving(Show)
+data Othello = Othello Board Ply Counts
+data Move = Move Coord [Coord] | Pass deriving(Show)
+
+instance Show Othello where
+    show (Othello board ply counts)=showBoard board++"\nply:"++show ply++",counts:"++show counts
 
 size :: (Coord,Coord)
 size = ((0,0),(7,7))
@@ -18,6 +25,10 @@ initialBoard = array size (zip (range size)$ repeat Nothing) // [((3,3),Just Whi
 
 initialOthello :: Othello
 initialOthello= Othello initialBoard 0 (2,2)
+
+showBoard :: Board->String
+showBoard b = Data.List.intercalate"\n"[concat[maybe "." show (b!(x,y))|y<-range (y1,y2)]|x<-range (x1,x2)]
+    where ((x1,y1),(x2,y2))=size
 
 inv :: Color->Color
 inv Black = White
@@ -39,8 +50,24 @@ canPut (x,y) color board = concat$ do
         f c = onBoard c && board!c == Just enemy
 
 getMoves :: Color->Board->[Move]
-getMoves color board = do
-    x<-[0..7]
-    y<-[0..7]
-    let canput = canPut (x,y) color board
-    if canput==[] then [] else [Move (x,y) canput]
+getMoves color board = if null ret then [Pass] else ret
+    where ret = do
+                  x<-[0..7]
+                  y<-[0..7]
+                  let canput = canPut (x,y) color board
+                      if canput==[] then [] else [Move (x,y) canput]
+
+turnColor :: Othello->Color
+turnColor (Othello _ ply _) = if rem ply 2==0 then Black else White
+
+doMove :: Move->Othello->Othello
+doMove Pass (Othello board p c) = Othello board (p+1) c
+doMove (Move put@(x,y) changes) o@(Othello board p c) = (Othello newboard (p+1) c)
+  where color=turnColor o
+        newboard = board // zip (put:changes) (repeat$ Just color)
+
+evaluate :: Othello->Int
+evaluate o@(Othello _ _ (cb,cw)) = (if gameEnd o then 100 else 1)*(cb-cw)
+
+gameEnd :: Othello->Bool
+gameEnd (Othello _ _ (cb,cw)) = cb+cw==64
