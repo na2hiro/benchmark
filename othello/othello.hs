2b52a9c6d3341303202a17aad91a0fd862354e33
na2hiro <a@81.la>
2013-11-09 02:13:23 +0900

    αβ

--- othello/othello.hs
+++ othello/othello.hs
@@ -77,7 +77,8 @@ evaluate o@(Othello _ _ (cb,cw)) = (if gameEnd o then 100 else 1)*(cb-cw)
 gameEnd :: Othello->Bool
 gameEnd (Othello _ _ (cb,cw)) = cb+cw==64
 
-data BestMove = BestMove Move Int Int deriving(Show)
+type Eval = Int
+data BestMove = BestMove Move Eval Int deriving(Show)
 
 minimax :: Int->Othello->BestMove
 minimax 0 game = BestMove Pass (evaluate game) 1
@@ -91,3 +92,21 @@ minimax depth game = foldr (f op) (BestMove Pass initEval 0) bms
           f op (BestMove mv1 ev1 q1) (BestMove mv2 ev2 q2) = if ev1 `op` ev2 
                                                                then BestMove mv1 ev1 (q1+q2)
                                                                else BestMove mv2 ev2 (q1+q2)
+
+alphabeta :: Int->Maybe BestMove->Othello->BestMove
+alphabeta 0 _ game = trace ("eval"++show game++show (evaluate game))$ BestMove Pass (evaluate game) 1
+alphabeta depth last game = either id id$ foldl (flip f) (Right (BestMove Pass initEval 0)) moves
+    where (op, initEval) = if turnColor game==Black then ((>=), minBound) else ((<=), maxBound)
+          moves = getMoves game
+          f :: Move->Either BestMove BestMove->Either BestMove BestMove
+          f _ l@(Left b) = l
+          f move (Right ret) = if cut then trace "cut"$ Left betterOne else Right betterOne 
+            where next = doMove move game
+                  best@(BestMove bmove beval bquant) = trace (show move)$ alphabeta (depth-1) (Just ret) next
+                  newbest = BestMove move beval bquant
+                  betterOne = better newbest ret
+                  cut = maybe False (\l->better newbest l`sameEval` newbest) last
+                  sameEval (BestMove _ e1 _) (BestMove _ e2 _) = e1==e2
+          better (BestMove mv1 ev1 q1) (BestMove mv2 ev2 q2) = if ev1 `op` ev2 
+                                                                 then BestMove mv1 ev1 (q1+q2)
+                                                                 else BestMove mv2 ev2 (q1+q2)
