9eac4c0b32c6bccfa6f89c791a793f2f94a8be63
Mamesoft <info@mamesoft.jp>
2014-07-15 22:09:07 -0700

    add Visual Basic

---
+++ Fibonacci/fib.vb
@@ -0,0 +1,16 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim n As Integer = 40
+        System.Console.WriteLine("Fib(" & n & ") = " & Fib(n))
+
+    End Sub
+    Function Fib(ByVal n As Integer) As Integer
+        If n = 0 OrElse n = 1 Then
+            Return 1
+        Else
+            Return Fib(n - 1) + Fib(n - 2)
+        End If
+    End Function
+
+End Module
